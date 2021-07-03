b579c54c80e54bce2e38ccd81482cea4a35918dd
NikitaMelnychencko <hunter6059@gmail.com>
2021-07-03 20:37:53 +0300

    intermediate version
    intermediate version

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './sass/main.scss';
+
+(() => {
+const menuBtnRef = document.querySelector("[data-menu-button]");
+const mobileMenuRef = document.querySelector("[data-menu]")
+
+menuBtnRef.addEventListener("click", () => {
+  const expanded =
+    menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+  
+  menuBtnRef.classList.toggle("menu-button--is-open");
+  menuBtnRef.setAttribute("aria-expanded", !expanded);
+  document.body.classList.toggle("modal-open");
+
+  mobileMenuRef.classList.toggle("menu-container-mobile--is-open");
+    
+});
+})();
