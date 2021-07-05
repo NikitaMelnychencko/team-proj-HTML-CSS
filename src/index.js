7d185cc1fce504f578d66ea2a237b1e0ecb9c7a2
NikitaMelnychencko <hunter6059@gmail.com>
2021-07-06 02:13:02 +0300

    +

--- src/index.js
+++ src/index.js
@@ -66,4 +66,21 @@ mobileBtnFour.addEventListener("click", () => {
   
   mobileMenuRef.classList.toggle("menu-container-mobile--is-open");
 });
+  
+const links = document.querySelectorAll(".page-header ul a");
+ 
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
  })();
